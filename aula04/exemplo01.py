4d2079dd06ce0e3739b08ae80dc1a1ab0d5c9c3f
luisaabarros <luisa.barros@aluno.senac.rio>
2025-11-25 21:23:57 -0300

    Acrescenta operador AND e OR

--- aula04/exemplo01.py
+++ aula04/exemplo01.py
@@ -10,11 +10,41 @@
 
 #Testando mais condições
 
-pontos = int(input('Quantos pontos foram feitos?'))
+# pontos = int(input('Quantos pontos foram feitos?'))
+# if pontos >=100:
+#    print('Nível máximo!')
+# elif pontos >=50:
+#    print('Nível bom...')
+# else:
+#    print('Pontuação baixa.')
 
-if pontos >=100:
-   print('Nível máximo!')
-elif pontos >=50:
-   print('Nível bom...')
+# Verificação de login
+
+# usuario = input('Informe o usuário: ').lower()
+# senha = input("Informe a senha: ")
+
+# if usuario == 'admin' and senha == '1234':
+#     print('Usuário e senha corretos!')
+# else:
+#     print('Ops! Usuário e senha incorretos!')
+
+# Condição para brinde
+
+# compra = float(input('Qual o valor da compra?'))
+# cliente_frequente = input('Cliente frequente? [S/N]').lower()
+
+# if compra >= 1000 or cliente_frequente == 's':
+#     print('Tem direito a brinde!')
+# else:
+#     print('Não tem direito a brinde.')
+
+nota = float(input('Informe sua nota: '))
+frequencia = float(input('Informe sua frequencia: '))
+
+if nota >= 7:
+    if frequencia >= 75:
+        print('Aprovado.')
+    else:
+        print('Reprovado por falta.')
 else:
-   print('Pontuação baixa.')
+    print('Reprovado por nota.')
